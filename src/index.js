773af05c7f16736dd8466778e3d012e0bf787293
Casimova Marina <maracasimova@gmail.com>
2023-04-05 22:22:55 +0300

    created partials, loaded information in html from task

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '35093909-87304c45613df24ba6fd5782f';
+const BASE_URL = 'https://pixabay.com/api/';
